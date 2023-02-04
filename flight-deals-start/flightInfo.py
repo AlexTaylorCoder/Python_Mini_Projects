9dcbd2b227036913cbc277268670a649a9a58797
alextaylor <alextaylor515@gmail.com>
2023-02-04 18:46:42 -0500

    Scraped link of airline's flight from google

--- flight-deals-start/flightInfo.py
+++ flight-deals-start/flightInfo.py
@@ -1,6 +1,7 @@
 from customtkinter import *
 from data_manager import DataManager
 import requests
+import webbrowser
 from bs4 import BeautifulSoup
 BASE_ENDPOINT = "https://www.google.com/search"
 class FlightInfo(CTkToplevel):
@@ -18,17 +19,23 @@ class FlightInfo(CTkToplevel):
 
         self.header.grid(row=0,column=3,columnspan=9,rowspan=3)
 
+            
+        self.linkButton = CTkButton(master=self.header,text="Airline Site",command=self.find_source)
+        self.save = CTkButton(master=self.header,text="Track",command=self.save_flight)
+
 
         datefromLabel = CTkLabel(master=self.header,text=self.dateFrom)
         datetoLabel = CTkLabel(master=self.header,text=self.dateTo)
         cityLabel = CTkLabel(master=self.header,text=self.cityTo)
         priceLabel = CTkLabel(master=self.header,text=f"${self.price}")
-        self.save = CTkButton(master=self.header,text="Track",command=self.save_flight)
+    
 
         datefromLabel.pack(side=LEFT,padx=5)
         datetoLabel.pack(side=LEFT,padx=5)
         cityLabel.pack(side=LEFT,padx=5)
         priceLabel.pack(side=LEFT,padx=5)
+
+        self.linkButton.pack(side=LEFT,padx=5)
         self.save.pack(side=LEFT,padx=10)
 
         for i,route in enumerate(flightData["path"]):
@@ -44,9 +51,23 @@ class FlightInfo(CTkToplevel):
         else:
             self.save.configure(text="DB Error!")
     def find_source(self):
+        self.linkButton.configure(text="Loading...")
+        #Need to fill in params
         params = {
             "q":f"{self.airline}+airlines+to+{self.cityTo}"
         }
-        response = requests.get(f"BASE_ENDPOINT",params=params)
+        headers = {
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+            "Accept-Language":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        }
+        response = requests.get(BASE_ENDPOINT,params=params,headers=headers)
+        airline_data = response.text
+        soup = BeautifulSoup(airline_data,"html.parser")
+        print(soup)
+        top_link = soup.select_one(".g a")["href"]
+        webbrowser.open_new_tab(top_link)
+
+        self.linkButton.configure(text="Airline Site")
+
         
 
